a63382fbaffd1e8bd77895a6c27027548fd5e830
Dave Van Veen <davemvanveen@gmail.com>
2018-08-12 18:56:50 -0500

    dip_estimator written, ready for test

--- cs_dip.py
+++ cs_dip.py
@@ -3,26 +3,13 @@ import parser
 import torch
 from torch.autograd import Variable
 
-import utils as utils
-from utils import DCGAN_XRAY
-from utils import DCGAN_MNIST
+import utils
+from utils import DCGAN_XRAY, DCGAN_MNIST
 
 
-args = parser.parse_args('configs.json')
-# CS-DIP hyperparameters
-LR = args.learning_rate
-MOM = args.momentum
-WD = args.weight_decay
-NUM_ITER = args.number_iterations
-NUM_RESTARTS = args.number_restarts
+args = parser.parse_args('configs.json') # contains neural net hyperparameters
 NGF = 64
-# Data-specific parameters
-Z_DIM = args.Z_DIM
-DATASET = args.dataset
 BATCH_SIZE = 1
-NUM_CHANNELS = args.NUM_CHANNELS
-IMG_SIZE = args.IMG_SIZE # pixel height/width, i.e. length of square image
-
 
 CUDA = torch.cuda.is_available()
 
@@ -35,79 +22,83 @@ else:
 
 se = torch.nn.MSELoss(reduce=False).type(dtype)
 
-z = Variable(torch.zeros(BATCH_SIZE*Z_DIM).type(dtype).view(BATCH_SIZE,Z_DIM,1,1))
-z.data.normal_().type(dtype)
-z.requires_grad = False
-
 mse_min = 0.
 begin_checkpoint = 50 # iteration at which to begin checking exit condition
 exit_window = 25 # number of consecutive MSE values upon which we compare
 
-mse_ = np.zeros((NUM_RESTARTS,BATCH_SIZE))
-meas_loss_ = np.zeros((NUM_RESTARTS,BATCH_SIZE))
-reconstructions_ = np.zeros((NUM_RESTARTS, BATCH_SIZE, NUM_CHANNELS, IMG_SIZE, IMG_SIZE))
+meas_loss_ = np.zeros((args.NUM_RESTARTS, args.BATCH_SIZE))
+reconstructions_ = np.zeros((args.NUM_RESTARTS, args.BATCH_SIZE, args.NUM_CHANNELS, \
+					args.IMG_SIZE, args.IMG_SIZE))
 
-def csdip_optimize(num_measurements, batch, label):
-	for j in range(NUM_RESTARTS):
-	    # initialize network
-	    if DATASET =='xray':
-	        net = DCGAN_XRAY(Z_DIM,NGF,IMG_SIZE,NUM_CHANNELS,num_measurements) 
-	    elif DATASET == 'mnist':
-	        net = DCGAN_MNIST(Z_DIM,NGF,IMG_SIZE,NUM_CHANNELS,num_measurements)
-	    net.fc.requires_grad = False
-	    net.fc.weight.data = torch.Tensor((1/np.sqrt(1.0*num_measurements))*\
-	    np.random.randn(num_measurements, IMG_SIZE*IMG_SIZE*NUM_CHANNELS)).type(dtype) # A[num_measurements]
+# Notes
+	# Nested functions? No special purpose. Just so we can get an 'estimator' f'n and see x-hat created in main
+	# Batch size / how to deal with batches - treat as 1 image; hard code BATCH_SIZE
+	# MSE calculated in separate plotting file from reading reconstructions
+	# Create file hierarchy with layers for (1) dataset (2) basis and (3) NUM_MEAS 
+	# Anything in dip_estimator we can cut down?
 
-	    reset_list = [net.conv1, net.bn1, net.conv2, net.bn2, net.conv3, net.bn3, \
-	                  net.conv4, net.bn4, net.conv5]
-	    for temp in reset_list:
-	       temp.reset_parameters()
 
-	    allparams = [temp for temp in net.parameters()]
-	    allparams = allparams[:-1] # get rid of last item in list (fc layer)
-	    
 
-	    z = Variable(torch.zeros(BATCH_SIZE*Z_DIM).type(dtype).view(BATCH_SIZE,Z_DIM,1,1))
-	    z.data.normal_().type(dtype)
+# TODO: file hierarchy!!! (see above)
+def dip_estimator(args):
+	def estimator(A_val, y_batch_val, args):
 
-	    if CUDA:
-	        net.cuda()
+		y = Variable(torch.Tensor(y_batch_val).type(dtype)) # cast measurements to GPU if possible
 
-	    batch, label = batch.type(dtype), label.type(ltype)
-	    batch_measurements = Variable(torch.mm(batch.view(BATCH_SIZE,-1),net.fc.weight.data.permute(1,0)),\
-	                             requires_grad=False) 
+		for j in range(NUM_RESTARTS):
+			if args.DATASET == 'xray':
+				net = DCGAN_XRAY(args.Z_DIM, NGF, args.IMG_SIZE,\
+					args.NUM_CHANNELS, args.NUM_MEASUREMENTS)
+			elif args.DATASET == 'mnist':
+				net = DCGAN_XRAY(args.Z_DIM, NGF, args.IMG_SIZE,\
+					args.NUM_CHANNELS, args.NUM_MEASUREMENTS)  
 
-	    optim = torch.optim.RMSprop(allparams,lr=LR, momentum=MOM, weight_decay=WD)
+			net.fc.requires_grad = False
+			net.fc.weight.data = torch.Tensor(A_val.T) # set A to be fc layer
 
-	    mse_temp = []
-	    for i in range(NUM_ITER):
-	        # TODO: Move optimizer to utils file
-	        optim.zero_grad()
-	        loss = torch.mean(torch.sum(se(net.measurements(z,batch_size=BATCH_SIZE),batch_measurements),dim=1))
 
-	        loss.backward()
-	        mse_value = np.mean(se(net(z),Variable(batch,requires_grad=False)).data.cpu().numpy(), axis=(1,2,3))
-	        mse_temp.append(mse_value[0])
-	        
-	        if (i >= begin_checkpoint): # if past a certain number of iterations
-	            # check to see if we've obtained the minimum MSE
-	            should_exit, mse_min_temp = utils.exit_check(mse_temp[-exit_window:],i)
-	            if should_exit == True:
-	                break
-	        else:
-	            should_exit = False
+			# We don't need this, correct?
+		    # reset_list = [net.conv1, net.bn1, net.conv2, net.bn2, net.conv3, net.bn3, \
+		    #               net.conv4, net.bn4, net.conv5] # dataset-dependent
+		    # for temp in reset_list:
+		    #    temp.reset_parameters()
 
-	        optim.step()
+		    allparams = [temp for temp in net.parameters()]
+		    allparams = allparams[:-1] # get rid of last item in list (fc layer)
+		    
+		    z = Variable(torch.zeros(BATCH_SIZE*args.Z_DIM).type(dtype).view(BATCH_SIZE,args.Z_DIM,1,1))
+		    z.data.normal_().type(dtype)
+		    z.requires_grad = False
 
-	    # if first 'restart', set mse_min OR if we've set a new low with future 'restart'
-	    if (j == 0) or (j > 0 and mse_min_temp < mse_min): 
-	        mse_min = mse_min_temp   
+		    if CUDA:
+		        net.cuda()
 
-	    # get reconstructions
-	    reconstructions_[j] = net(z).data.cpu().numpy()
-	    # get mse of reconstructions
-	    mse_[j] = mse_min_temp
-	    # get measurement losses
-	    meas_loss_[j] = np.sum(se(net.measurements(z,batch_size=BATCH_SIZE),batch_measurements).data.cpu().numpy(),axis=1)
+		    optim = torch.optim.RMSprop(allparams,lr=args.LR, momentum=args.MOM, weight_decay=args.WD)
 
-	return reconstructions_, mse_, meas_loss_
+		    loss_temp = [] 
+		    for i in range(NUM_ITER):
+
+		        optim.zero_grad()
+		        loss = torch.mean(torch.sum(se(net.measurements(z,batch_size=BATCH_SIZE),y),dim=1))
+		        loss.backward()
+		        
+		        meas_loss = np.sum(se(net.measurements(z,batch_size=BATCH_SIZE),y).data.cpu().numpy(),axis=1)
+		        loss_temp.append(meas_loss) # save loss value of each iteration to array
+		        
+		        if (i >= begin_checkpoint): # if optimzn has converged, exit descent
+		            should_exit, loss_min_restart = utils.exit_check(loss_temp[-exit_window:],i)
+		            if should_exit == True:
+		            	meas_loss = loss_min_restart # get first loss value of exit window
+		                break
+		        else:
+		            should_exit = False
+
+		        optim.step()  
+
+		    reconstructions_[j] = net(z).data.cpu().numpy() # get reconstructions	    
+		    meas_loss_[j] = meas_loss # get last measurement loss for a given restart
+
+		idx_best = np.argmin(meas_loss_,axis=0) # index of restart with lowest loss
+		x_hat = reconstructions_[ind_best] # choose best reconstruction from all restarts
+
+	return x_hat
