33faa29449fbe691f4dee5ce82ec450b7afed6ba
Dave Van Veen <davemvanveen@gmail.com>
2018-08-16 17:14:20 -0500

    pipeline finalized

--- comp_sensing.py
+++ comp_sensing.py
@@ -6,28 +6,22 @@ import parser
 import torch
 from torchvision import datasets
 
-import cs_dip as csd
+import cs_dip
 import utils
 import baselines
 
 args = parser.parse_args('configs.json')
-print(args)
-DATASET = args.dataset
-DATA_DIR = args.DATA_DIR
-NET_PATH = args.NET_PATH
-NUM_CHANNELS = args.NUM_CHANNELS
-IMG_SIZE = args.IMG_SIZE # pixel height/width, i.e. length of square image
 
 
-NUM_MEASUREMENTS = args.NUM_MEASUREMENTS # TODO: cycle this over a list, add user input
-NUM_MEASUREMENTS_LIST = [NUM_MEASUREMENTS] # number of measurements to iterate over
+NUM_MEASUREMENTS_LIST = args.NUM_MEASUREMENTS # TODO: cycle this over a list, add user input
+if isinstance(args.NUM_MEASUREMENTS, int): # if looking at single value for NUM_MEAS, cast to list
+    NUM_MEASUREMENTS_LIST = [NUM_MEASUREMENTS_LIST]
 
 BATCH_SIZE = 1
+#BASIS = ['csdip'] # method:  put this in config so they can loop through list
 
-BASIS = ['dip'] # method:  put this in config so they can loop through list
 
-
-compose = utils.define_compose(NUM_CHANNELS, IMG_SIZE)
+compose = utils.define_compose(args.NUM_CHANNELS, args.IMG_SIZE)
 # DOWNLOAD 100 MNIST IMAGES AND SAVE IN REPO, don't use Torch's MNIST loader
 # dataloader = torch.utils.data.DataLoader(datasets.MNIST('../data/',download=True, \
 # 				train=TRAIN,transform=compose), shuffle=False, batch_size=BATCH_SIZE)
@@ -36,66 +30,33 @@ dataset = datasets.ImageFolder('data/tomo/', transform = compose)
 dataloader = torch.utils.data.DataLoader(dataset, shuffle=False, batch_size=BATCH_SIZE)
 
 
-RECONSTRUCTIONS = dict()
-MSE = dict() # CALUCATE LATER IN PLOT FILE
-
-#A = baselines.get_A(args.IMG_SIZE*args.IMG_SIZE*args.NUM_CHANNELS, num_measurements)
-#y = np.dot(x,A)
-
-for basis in BASIS: # DO WE NEED THIS?
-    '''
-    if basis == 'csdip':
-        estimator = cs_dip.dip_estimator(args)
-    elif basis == 'dct':
-        estimator = baselines.lasso_dct_estimator(args)
-    elif basis == 'wavelet':
-        estimator = baselines.lasso_wavelet_estimator(args)
-    else:
-        raise NotImplementedError
-    x_hat = estimator(A,y,args)
-    '''
-    for num_measurements in NUM_MEASUREMENTS_LIST: #NEED TO ADD LIST FUNCTIONALITY LATER
-        print('Number of measurements: ' + str(num_measurements))
-        # check if the pickle files already exist. if they do, update the respective
-        # empty dictionaries using contents of the files
-        rec_f = '{0}_reconstructions_{1}.pkl'.format(basis,num_measurements)
 
 
-        # rec_f = 'reconstructions/{0}/{1}'.format(basis,num_measurements) also dataset
+for num_measurements in NUM_MEASUREMENTS_LIST:
+    
+    args.NUM_MEASUREMENTS = num_measurements
+    A = baselines.get_A(args.IMG_SIZE*args.IMG_SIZE*args.NUM_CHANNELS, args.NUM_MEASUREMENTS)
 
-        # save x_hat in numpy file? nested directories OR pickle files
-            # save numpy files in nested directories since hierarchy is simple
-            # ISSUE: need filenames of images; or just save with index created based upon batch
-            # fix file
+    for _, (batch, _) in enumerate(dataloader):
 
-        if os.path.exists(rec_f):
-            with open(rec_f,'rb') as f:
-                temp = pkl.load(f)
-                RECONSTRUCTIONS.update(temp)
+        x = batch.view(BATCH_SIZE, -1).cpu().numpy()
+        y = np.dot(x,A)
 
+        for basis in args.BASIS: # check functionality for different bases
+            # print(args.BASIS)
+            # print(basis)
+            
+            if basis == 'csdip':
+                estimator = cs_dip.dip_estimator(args)
+            elif basis == 'dct':
+                estimator = baselines.lasso_dct_estimator(args)
+            elif basis == 'wavelet':
+                estimator = baselines.lasso_wavelet_estimator(args)
+            else:
+                raise NotImplementedError
 
-        if num_measurements in RECONSTRUCTIONS.keys():
-            pass
-        else:
-            RECONSTRUCTIONS[num_measurements] = []
+            x_hat = estimator(A,y,args)
 
-        for k, (batch, label) in enumerate(dataloader):
-            if k>=100:
-                break            
+            utils.save_reconstruction(x_hat, args)
 
-            # DIP specific, e.g. if basis == 'dip'
-            reconstructions_, mse_, meas_loss_ = csd.csdip_optimize(num_measurements, batch, label)
-            idx = np.argmin(mse_,axis=0)
-
-
-
-            for j in range(BATCH_SIZE):
-                RECONSTRUCTIONS[num_measurements].append(reconstructions_[idx[j],j])
-
-                print('Image ' + str(k+1))
-                    
-        # pickle the dictionaries
-        with open(rec_f,'w') as f:
-            pkl.dump(RECONSTRUCTIONS, f)
-
-        print('Done!')
+print('Done!')
