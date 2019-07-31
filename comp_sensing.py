ecfb64e5b82527e6c8d8b78734be9048699ef895
vanveenthedream <davemvanveen@gmail.com>
2019-07-31 00:30:15 +0000

    updated for noisy experiments

--- comp_sensing.py
+++ comp_sensing.py
@@ -18,6 +18,7 @@ args = parser.parse_args('configs.json')
 print(args)
 
 NUM_MEASUREMENTS_LIST, ALG_LIST = utils.convert_to_list(args)
+NOISE_LIST = [0, 0.1, 1, 5, 10, 20, 50, 100]
 
 dataloader = utils.get_data(args) # get dataset of images
 
@@ -29,36 +30,39 @@ for num_meas in NUM_MEASUREMENTS_LIST:
     
     for _, (batch, _, im_path) in enumerate(dataloader):
 
+        for noise in NOISE_LIST:
+            args.NOISE = noise # set value to induce noise
+            mmm = args.NUM_MEASUREMENTS 
+            # second argument is for np.random.normal is \sigma, i.e. std devn
+            # thus feed in \sigma / sqrt(m) to get variance of \sigma^2 / m
+            eta = np.random.normal(0, noise /  np.sqrt(mmm) , mmm)
         
-        eta_sig = 0 # set value to induce noise 
-        eta = np.random.normal(0, eta_sig * (1.0 / args.NUM_MEASUREMENTS) ,args.NUM_MEASUREMENTS)
-        
 
-        x = batch.view(1,-1).cpu().numpy() # define image
-        y = np.dot(x,A) + eta
+            x = batch.view(1,-1).cpu().numpy() # define image
+            y = np.dot(x,A) + eta
 
-        for alg in ALG_LIST:
-            args.ALG = alg
+            for alg in ALG_LIST:
+                args.ALG = alg
 
-            if utils.recons_exists(args, im_path): # to avoid redundant reconstructions
-                continue
-            NEW_RECONS = True
+                if utils.recons_exists(args, im_path): # to avoid redundant reconstructions
+                    continue
+                NEW_RECONS = True
 
-            if alg == 'csdip':
-                estimator = cs_dip.dip_estimator(args)
-            elif alg == 'dct':
-                estimator = baselines.lasso_dct_estimator(args)
-            elif alg == 'wavelet':
-                estimator = baselines.lasso_wavelet_estimator(args)
-            elif alg == 'bm3d' or alg == 'tval3':
-                raise NotImplementedError('BM3D-AMP and TVAL3 are implemented in Matlab. \
+                if alg == 'csdip':
+                    estimator = cs_dip.dip_estimator(args)
+                elif alg == 'dct':
+                    estimator = baselines.lasso_dct_estimator(args)
+                elif alg == 'wavelet':
+                    estimator = baselines.lasso_wavelet_estimator(args)
+                elif alg == 'bm3d' or alg == 'tval3':
+                    raise NotImplementedError('BM3D-AMP and TVAL3 are implemented in Matlab. \
                                             Please see GitHub repository for details.')
-            else:
-                raise NotImplementedError
+                else:
+                    raise NotImplementedError
 
-            x_hat = estimator(A, y, args)
+                x_hat = estimator(A, y, args)
 
-            utils.save_reconstruction(x_hat, args, im_path)
+                utils.save_reconstruction(x_hat, args, im_path)
 
 if NEW_RECONS == False:
     print('Duplicate experiment configurations. No new data generated.')
